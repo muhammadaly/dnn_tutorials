03a90bb178109d7d33733afe21a584867aaa2ebe
muhammadaly-iv <muhammad.aly@ivsystems-eg.com>
2016-12-01 15:48:09 +0200

    add tf)simplecnn_minst.py

--- Post1/tf_multinational_logistic.py
+++ Post1/tf_multinational_logistic.py
@@ -1,3 +1,6 @@
+#https://www.tensorflow.org/versions/r0.12/tutorials/mnist/pros/index.html#deep-mnist-for-experts
+#https://classroom.udacity.com/courses/ud730/lessons/6379031992/concepts/64036785920923#
+
 import tensorflow as tf
 
 if __name__ == '__main__'
